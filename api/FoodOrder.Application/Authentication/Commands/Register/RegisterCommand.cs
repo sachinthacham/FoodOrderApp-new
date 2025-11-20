3e52397f2cd99111be64ee296a48af499ba22d3f
sachinthacham <sachinthachamindu26@gmail.com>
2025-11-20 11:28:13 +0530

    implement register command

---
+++ api/FoodOrder.Application/Authentication/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,12 @@
+using FoodOrder.Application.Authentication.Common;
+using ErrorOr;
+using MediatR;
+
+namespace FoodOrder.Application.Authentication.Commands.Register;
+
+public record RegisterCommand(
+    string FirstName,
+    string LastName,
+    string Email,
+    string Password
+) : IRequest<ErrorOr<AuthenticationResult>>;
