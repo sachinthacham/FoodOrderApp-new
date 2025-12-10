a8ada7a06858ccdf55a0f7b18bf0d8c6579a594a
sachinthacham <sachinthachamindu26@gmail.com>
2025-12-10 22:30:17 +0530

    implemt order repository interface

--- api/FoodOrder.Application/common/Interfaces/Persistence/IOrderRepository.cs
+++ api/FoodOrder.Application/common/Interfaces/Persistence/IOrderRepository.cs
@@ -1,10 +1,18 @@
 using FoodOrder.Domain.Entities;
+using FoodOrder.Domain.Common;
 
 namespace FoodOrder.Application.common.Interfaces.Persistence
 {
     public interface IOrderRepository
     {
         Task AddAsync(Order order);
+        Task<Order?> GetByIdAsync(Guid id);
         Task<List<Order>> GetByUserIdAsync(Guid userId);
+        Task<List<Order>> GetAllAsync();
+        Task<List<Order>> GetByRestaurantIdAsync(Guid restaurantId);
+        Task<List<Order>> GetByRestaurantIdsAsync(List<Guid> restaurantIds);
+        Task<List<Order>> GetByDeliveryBoyIdAsync(Guid deliveryBoyId);
+        Task<List<Order>> GetByStatusAsync(OrderStatus status);
+        Task UpdateAsync(Order order);
     }
 }
