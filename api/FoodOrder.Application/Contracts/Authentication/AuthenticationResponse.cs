1375bec252e4c409b9046670edaaacfd560d8ff3
sachinthacham <sachinthachamindu26@gmail.com>
2025-12-11 13:45:21 +0530

    implement authentication response

---
+++ api/FoodOrder.Application/Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+namespace FoodOrder.Application.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Role,
+    string Token
+);
