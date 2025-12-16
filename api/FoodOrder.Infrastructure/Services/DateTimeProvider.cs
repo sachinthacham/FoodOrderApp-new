cf8bd403864d5327b2191151c60a082c4e137712
sachinthacham <sachinthachamindu26@gmail.com>
2025-12-16 08:31:17 +0530

    implement datatime provider

---
+++ api/FoodOrder.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+using FoodOrder.Application.Common.Interfaces.Services;
+
+namespace FoodOrder.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
+
