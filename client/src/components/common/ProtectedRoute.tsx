f2bbd2b037987da2ffa064c4281b1c6103832ce0
sachinthacham <sachinthachamindu26@gmail.com>
2025-12-21 23:12:21 +0530

    impement protect routing

---
+++ client/src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "@/store/useAuthStore";
+import type{  UserRole } from "@/store/useAuthStore";
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+  allowedRoles?: UserRole[];
+}
+
+export default function ProtectedRoute({
+  children,
+  allowedRoles,
+}: ProtectedRouteProps) {
+  const { isAuthenticated, user, hasRole } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  if (allowedRoles && !hasRole(allowedRoles)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+}
